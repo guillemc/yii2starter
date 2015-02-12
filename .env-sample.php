01d640a452cf7e5eeae1d3728b575dba84f98bbc
Guillem Comes <guillemc@gmail.com>
2015-02-13 00:09:31 +0100

    Add .gitignore and .env-sample.php

---
+++ .env-sample.php
@@ -0,0 +1,11 @@
+<?php
+
+define('YII_DEBUG', true);
+define('YII_ENV', 'dev');
+
+define('COOKIE_VALIDATION_KEY', 'some random string - CHANGE IT!');
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'testdrive');
+define('DB_USER', 'root');
+define('DB_PASS', '');
